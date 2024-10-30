d998e8455cfa4c74e6aaa9f00b9a6fb5e3104155
BlackTurbo <129968533+sohanksome@users.noreply.github.com>
2024-10-31 05:31:52 +0800

    delete logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -23,11 +23,6 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
-        <Image src='/assets/images/logo.svg'
-          alt='Logo'
-          width={30}
-          height={30}
-          className='object-contain' />
         <p className='logo_text'>Prompt house</p>
       </Link>
 
