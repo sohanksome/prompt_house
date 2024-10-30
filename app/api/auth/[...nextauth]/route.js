02220b6f390e772204f158a33457c189b4ed90fa
BlackTurbo <129968533+sohanksome@users.noreply.github.com>
2024-10-31 07:19:20 +0800

    polish for a little bit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,7 @@
 import NextAuth from 'next-auth'
-import Google from 'next-auth/providers/google';
 import GoogleProvider from 'next-auth/providers/google';
 import User from '@models/user';
 import { connectToDB } from '@utils/database'
-
-console.log({
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-})
 
 const handler = NextAuth({
     providers: [
--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,13 +1,8 @@
 import { connectToDB } from '@utils/database'
 import Prompt from '@models/prompt'
-
-
-
-
 export const GET = async (request, { params }) => {
     try {
         await connectToDB();
-
         const prompts = await Prompt.findById(params.id).populate('creator')
         if (!prompts) return new Response('prompt not found', { status: 404 })
         return new Response(JSON.stringify(prompts), {
@@ -32,8 +27,7 @@ export const PATCH = async (request, { params }) => {
         existingPrompt.tag = tag
         await existingPrompt.save()
         return new Response(JSON.stringify(existingPrompt), { status: 200 })
-
-
+        
     } catch (error) {
         return new Response('update failed', { status: 500 })
     }
