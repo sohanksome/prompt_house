493e17d5f35e839abc013d77eb3022ed6d1d0e4a
BlackTurbo <129968533+sohanksome@users.noreply.github.com>
2024-10-31 05:43:04 +0800

    update page fixed again

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 import { Suspense, useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Form from '@components/Form';
